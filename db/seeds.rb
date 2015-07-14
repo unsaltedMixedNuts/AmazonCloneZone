b38c916cbc67f9251c34e9f9cc94adcd285e3372
Mel Muhammad <muhammad.mel@gmail.com>
2015-07-14 12:07:37 -0400

    Add seeds for database tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: "Apple TV MD199LL/A (Current Version)", sales_price: 59.99, quantity: 100, dept_id: 1, brand_id: 1 )
+Product.create(name: "Roku 3 Streaming Media Player (4230R) With Voice Search (2015 model)", sales_price: 99.00, quantity: 100, dept_id: 1, brand_id: 2 )
+
+Department.create(name: "Electronics", parent_dept_id: null)
+Department.create(name: "Television & Video", parent_dept_id: 1)
+
+Brand.create(name: Apple)
+Brand.create(name: Roku)
