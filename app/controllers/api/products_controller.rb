a73375e824f6ddb5aedf59c6b125d212a4cade41
Mel <Muhammad.Mel@gmail.com>
2015-08-15 20:40:41 -0400

    Start process of implementing Backbone.

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,12 @@
+class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "products#index"
+  # root to: "products#index"
+  root to: "site#root"
 
   resource :session, only: [:new, :create, :destroy]
   resource :cart, only: [:show]
@@ -10,4 +11,8 @@ Rails.application.routes.draw do
   resources :carted_items, only: [:create, :destroy]
   resources :orders, only: [:index, :create, :show]
   resources :questions, only: [:create]
+
+  namespace :api do
+    resources :products, only: [:index, :show]
+  end
 end
