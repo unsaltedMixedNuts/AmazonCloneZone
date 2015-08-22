420662a832c3f62cfca372ac478a97783654e0e1
Mel <Muhammad.Mel@gmail.com>
2015-08-21 22:43:00 -0400

    Add Sign In Page

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -5,7 +5,8 @@ AmazonCloneZone.Routers.Router = Backbone.Router.extend({
 
   routes: {
     "":"productsIndex",
-    "products/:id": "productsShow"
+    "products/:id": "productsShow",
+    "session/new": "signIn"
   },
 
   productsIndex: function () {
@@ -18,6 +19,39 @@ AmazonCloneZone.Routers.Router = Backbone.Router.extend({
     var model = AmazonCloneZone.Collections.products.getOrFetch(id);
     var view = new AmazonCloneZone.Views.ProductShow({ model: model });
     this._swapView(view);
+  },
+
+  signIn: function(callback){
+    if (!this._requireSignedOut(callback)) { return; }
+
+    var signInView = new AmazonCloneZone.Views.SignIn({
+      callback: callback
+    });
+    this._swapView(signInView);
+  },
+
+  _requireSignedIn: function(callback){
+    if (!AmazonCloneZone.currentUser.isSignedIn()) {
+      callback = callback || this._goHome.bind(this);
+      this.signIn(callback);
+      return false;
+    }
+
+    return true;
+  },
+
+  _requireSignedOut: function(callback){
+    if (AmazonCloneZone.currentUser.isSignedIn()) {
+      callback = callback || this._goHome.bind(this);
+      callback();
+      return false;
+    }
+
+    return true;
+  },
+
+  _goHome: function(){
+    Backbone.history.navigate("", { trigger: true });
   },
 
   _swapView: function (view) {
