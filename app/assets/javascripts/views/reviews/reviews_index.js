dcc8558ea5f1e120fa3cc4c97a9f5fb839e815da
Mel <Muhammad.Mel@gmail.com>
2015-08-31 13:19:27 -0400

    Create ability to leave reviews.

--- app/assets/javascripts/views/reviews/reviews_index.js
+++ app/assets/javascripts/views/reviews/reviews_index.js
@@ -18,12 +18,43 @@ AmazonCloneZone.Views.ReviewsIndex = Backbone.View.extend({
 
   submitNewReview: function (event) {
     event.preventDefault();
-    var params = $(event.currentTarget).serializeJSON()["review"];
-    var newReview = new AmazonCloneZone.Models.Review(params);
+    var params = $(event.currentTarget).serializeJSON();
+    var $message = $("<ul>");
+
+    if (params.review.stars == 0) {
+      this.reviewError = true;
+      $message.append("<li>Your product review must include a product rating.</li>")
+    }
+
+    if (params.review.title == "") {
+      this.reviewError = true;
+      $message.append("<li>Your product review must include a title.</li>")
+    }
+
+    if (params.review.body == "") {
+      this.reviewError = true;
+      $message.append("<li>Your product review must include content in addition to a rating and title.</li>")
+    }
+
+    if (this.reviewError) {
+      $message.append("Please submit your review after fixing the above issues.")
+      $("#flash-errors-for-reviews").html($message);
+      this.reviewError = false;
+      window.scrollTo(0,document.body.scrollHeight);
+      return;
+    }
+
+    var newReview = new AmazonCloneZone.Models.Review(params.review);
     var view = this;
     newReview.save([], {
       success: function () {
         view.product.reviews().add(newReview);
+        window.scrollTo(0,document.body.scrollHeight);
+        $("#flash-errors-for-reviews").html("Your review has been successfully posted and saved above.");
+      },
+
+      error: function () {
+        $("#flash-errors-for-reviews").html("Error: review not saved.");
       }
     });
   }
